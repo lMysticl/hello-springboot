9b356b999d6c48791aa37edbdd451e3c1c1594e5
lMysticl <putrenkov99@gmail.com>
2017-08-24 05:56:59 +0300

    -refresh

--- src/main/java/com/hellokoding/springboot/WebApplication.java
+++ src/main/java/com/hellokoding/springboot/WebApplication.java
@@ -2,10 +2,11 @@ package com.hellokoding.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 
 
-
-@SpringBootApplication
+@SpringBootApplication(exclude = {MongoAutoConfiguration.class, MongoDataAutoConfiguration.class})
 public class WebApplication {
 
     public static void main(String[] args) {
